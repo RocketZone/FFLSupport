a353e4f7473ee030babe79d1e1d2a03e4d11aa77
RocketZone <Conradza@hotmail.com>
2018-01-29 13:44:15 -0800

    PS1 v1 upload

---
+++ Scripts/Upload.ps1
@@ -0,0 +1,20 @@
+Remove-Item C:\Users\Conra\Desktop\125\* -Recurse -verbose
+    
+    Write-Host "Press any key to continue ..."
+    $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+Copy-Item C:\Users\Conra\Downloads\125Test\* C:\Users\Conra\Desktop\125 -verbose
+
+
+
+
+
+
+
+
+
+
+
+
+
+
